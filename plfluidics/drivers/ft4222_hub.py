05f5f40ff6ff56bc4a5b109a532b40d8915c1ab0
Robert R. Puccinelli <robert.puccinelli@outlook.com>
2026-05-19 10:12:27 -0700

    Restore code to windows machine - IO errors

--- plfluidics/drivers/ft4222_hub.py
+++ plfluidics/drivers/ft4222_hub.py
@@ -5,6 +5,7 @@ from ft4222.SPIMaster import Mode, Clock, SlaveSelect
 
 logger = logging.getLogger(__name__)
 
+
 class FT4222Hub():
     def __init__(self):
 #        self.logger = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
@@ -12,9 +13,21 @@ class FT4222Hub():
         self.device_details = {}
 
     def __del__(self):
-        for subunit in self.subunits:
+        self.close()
+
+    def close(self):
+        for subunit, dev in list(self.subunits.items()):
             logger.debug(f'Closing FT4222 device: {subunit}')
-            self.subunits[subunit] = []
+            try:
+                if hasattr(dev, 'close'):
+                    dev.close()
+                elif hasattr(dev, 'device') and hasattr(dev.device, 'close'):
+                    dev.device.close()
+            except Exception:
+                logger.exception(f'Error closing FT4222 subunit {subunit}')
+            finally:
+                self.subunits[subunit] = None
+        self.subunits.clear()
 
     def detectDevices(self):
         self.device_details = {}
@@ -29,16 +42,26 @@ class FT4222Hub():
             self.device_details[str(i)] = ft4222.getDeviceInfoDetail(i)
             logger.debug(f'Device {i}: {self.device_details[str(i)]}')
 
-    def initSPIDevice(self,
-                      device_id,
-                      mode=1,
+    def initSPIDevice(self, 
+                      device_id, 
+                      mode=1, 
                       clock=Clock.DIV_32,  # 1.875 MHz
-                      clock_pol=Cpol.IDLE_LOW,
-                      clock_phase=Cpha.CLK_TRAILING,
+                      clock_pol=Cpol.IDLE_LOW, 
+                      clock_phase=Cpha.CLK_TRAILING, 
                       slave_select=0):
         logger.debug(f'Opening device : {device_id}. Mode={mode}, clk={clock}, clk_pl={clock_pol}, clk_ph={clock_phase}, ss={slave_select}')
+        if device_id in self.subunits:
+            logger.debug(f'Reinitializing existing FT4222 subunit: {device_id}')
+            try:
+                self.subunits[device_id].close()
+            except Exception:
+                logger.exception(f'Error closing existing FT4222 subunit {device_id}')
+            finally:
+                self.subunits.pop(device_id, None)
+
         device = self._openDevice(device_id)
-        if  device != None:
+        spi_device = None
+        if device is not None:
             if slave_select == 0:
                 ss = SlaveSelect.SS0
             elif slave_select == 1:
@@ -50,39 +73,103 @@ class FT4222Hub():
             if mode == 1:
                 spi_device = FT4222SPIDevice_Single(device=device,
                                                     clock=clock,
-                                                    clock_pol=clock_pol,
-                                                    clock_phase=clock_phase,
+                                                    clock_pol=clock_pol, 
+                                                    clock_phase=clock_phase, 
                                                     slave_select=ss)
-
             self.subunits[device_id] = spi_device
         else:
-            print('Device not found')
             logger.warning(f'Device ID not found: `{device_id}`')
         return spi_device
+
+    def initGPIODevice(self,
+                       device_id,
+                       outputs,
+                       suspend=False,
+                       wake=False):
+        if device_id in self.subunits:
+            logger.debug(f'Reinitializing existing FT4222 GPIO subunit: {device_id}')
+            try:
+                self.subunits[device_id].close()
+            except Exception:
+                logger.exception(f'Error closing existing FT4222 GPIO subunit {device_id}')
+            finally:
+                self.subunits.pop(device_id, None)
+
+        device = self._openDevice(device_id)
+        gpio_device = None
+        if device is not None:
+            out_list = []
+            for i in range(4):
+                out_list.append(1 if i in outputs else 0)
+
+            gpio_device = FT4222GPIODevice(device, out_list, suspend=suspend, wake=wake)
+            self.subunits[device_id] = gpio_device
+        else:
+            logger.warning(f'Device ID not found: `{device_id}`')
+        return gpio_device
+    
+    def _openDevice(self, device_id):
+        device = None
+        try:
+            serials = [self.device_details[item]['serial'] for item in self.device_details]
+            descriptions = [self.device_details[item]['description'] for item in self.device_details]
+            if device_id.encode('utf-8') in serials:
+                device = ft4222.openBySerial(device_id)
+                logger.debug(f'FT4222 device opened by serial: `{device_id}`')
+            elif device_id.encode('utf-8') in descriptions:
+                device = ft4222.openByDescription(device_id)
+                logger.debug(f'FT4222 device opened by description: `{device_id}`')
+
+            if device is not None:
+                device.setTimeouts(5000, 5000)
+                try:
+                    device.setLatencyTimer(500)
+                except Exception:
+                    logger.debug('Unable to set latency timer; continuing without it.')
+        except Exception as e:
+            logger.exception(f'Unable to open FT4222 device {device_id}: {e}')
+            device = None
+        return device
+    
+    def test(self):
+        print('test')
     
 
 class FT4222SPIDevice_Single():
     def __init__(self,
-                 device: ft4222.FT4222,
+                 device: ft4222.FT4222, 
                  clock=Clock.DIV_32,  # 1.875 MHz
-                 clock_pol=Cpol.IDLE_LOW,
-                 clock_phase=Cpha.CLK_TRAILING,
+                 clock_pol=Cpol.IDLE_LOW, 
+                 clock_phase=Cpha.CLK_TRAILING, 
                  slave_select=SlaveSelect.SS0):
         logger = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
         self.device = device
         try:
             self.device.spiMaster_Init(clock=clock,
-                                    mode=Mode.SINGLE,
-                                    cpol=clock_pol,
-                                    cpha=clock_phase,
+                                    mode=Mode.SINGLE, 
+                                    cpol=clock_pol, 
+                                    cpha=clock_phase, 
                                     ssoMap=slave_select)
         except Exception as e:
-            self.__close__()
+            self.close()
             raise e
         
-    def __close__(self):
-        self.device.close()
-
+    def close(self):
+        if self.device is None:
+            return
+        try:
+            self.device.spiMaster_EndTransaction()
+        except Exception:
+            pass
+        try:
+            self.device.chipReset()
+        except Exception:
+            pass
+        try:
+            self.device.close()
+        except Exception:
+            pass
+        self.device = None
 
     def read(self, num_bytes=2, term=True):
         return self.device.spiMaster_SingleRead(bytesToRead=num_bytes, isEndTransaction=term)
@@ -94,10 +181,10 @@ class FT4222SPIDevice_Single():
         return self.device.spiMaster_SingleReadWrite(data=data, isEndTransaction=term)
 
 class FT4222GPIODevice():
-    def __init__(self,
-                 device: ft4222.FT4222,
-                 output_list,
-                 suspend=False,
+    def __init__(self, 
+                 device: ft4222.FT4222, 
+                 output_list, 
+                 suspend=False, 
                  wake=False):
         pin_dirs = []
         for gpio in output_list:
@@ -125,3 +212,13 @@ class FT4222GPIODevice():
             port = ft4222.GPIO.Port.P2
         elif pin == 3:
             port = ft4222.GPIO.Port.P3
+        self.device.gpio_Write(port, state)
+
+    def close(self):
+        if self.device is None:
+            return
+        try:
+            self.device.close()
+        except Exception:
+            pass
+        self.device = None
